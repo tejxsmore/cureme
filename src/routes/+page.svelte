c44ebbf4e77932ebb26ddc1700eef89a0b79f1fd
tejxsmore <tejasmore1104@gmail.com>
2024-02-09 00:59:36 +0530

    Dropdown added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
-<!-- src/routes/+page.svelte -->
 <script>
-	import { invalidateAll } from '$app/navigation';
-	import { appwrite } from '$lib/appwrite.js';
+    import { invalidateAll } from '$app/navigation';
+	import { appwrite } from "$lib/appwrite"
+    import { Dropdown, DropdownItem} from 'flowbite-svelte';
 
-	export let data;
-
+    export let data;
 	$: loggedIn = !!data.account;
 
 	async function logout() {
@@ -13,21 +12,34 @@
 		// In our case, this means we'll fetch the account data again.
 		await invalidateAll();
 	}
-	let modal = false;
+
 </script>
 
 {#if loggedIn}
-	<div class="bg-dark text-white p-4 flex justify-between items-center">
-		<h3 class="font-semibold">Commune.</h3>
-		<button on:click={logout} class="bg-red text-white w-6 h-6 rounded-full ">
-			{data.account?.name.charAt(0)}
-		</button>
-	</div>
+<div class="bg-dark text-white p-4 flex justify-between items-center">
+    <h3 class="font-semibold">Commune.</h3>
+    <button id="avatar" class="bg-red text-white w-6 h-6 rounded-full ">
+        {data.account?.name.charAt(0)}
+    </button>
+    <Dropdown
+        class="text-center text-black p-2 w-[150px] space-y-2 rounded-lg"
+        frameClass="rounded-lg"
+        placement="bottom"
+        triggeredBy="#avatar"
+    >
+        <DropdownItem class="font-semibold" href="/profile" >
+            Profile
+        </DropdownItem>
+        <hr>
+        <DropdownItem class="text-red font-semibold" on:click={logout}>
+            Signout
+        </DropdownItem>
+    </Dropdown> 
+    </div>
 {:else}
 	
 	<div class="bg-dark text-white p-4 flex justify-between">
-		<h3 class="font-semibold">Commune.</h3>
+		<h3 class="font-semibold">Dev Commune.</h3>
 		<a href="/login" class="">Login</a>
-		
 	</div>
 {/if}
