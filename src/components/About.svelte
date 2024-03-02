efbae23f1e294670309886689bc4dccbc0f0f53e
tejxsmore <tejasmore1104@gmail.com>
2024-03-02 15:07:10 +0530

    STOP

--- src/components/About.svelte
+++
@@ -1,3 +0,0 @@
-<div class="bg-light text-dark p-5 text-center">
-    <p>Health is the greatest of all possessions</p>
-</div>
--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,7 +1,5 @@
 <script>
-    import About from "./About.svelte";
 </script>
 
 <div class="bg-light text-dark z-10 min-h-screen">
-    <About/>
 </div>
